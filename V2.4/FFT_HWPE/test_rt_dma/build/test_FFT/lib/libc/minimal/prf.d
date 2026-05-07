6d4bf4e10df0f62deadb889179647c0b2276305a
Abdelhay <abdelhaymohamed21@gmail.com>
2026-05-07 23:00:22 +0300

    After adding dma and fft working synchronization

---
+++ V2.4/FFT_HWPE/test_rt_dma/build/test_FFT/lib/libc/minimal/prf.d
@@ -0,0 +1,14 @@
+/home/abdelhay21/GP/rtl/my_ips/FFT_HWPE/test_rt_dma/build/test_FFT/lib/libc/minimal/prf.o: \
+ /home/abdelhay21/GP/pulp-runtime/lib/libc/minimal/prf.c \
+ /home/abdelhay21/GP/pulp-runtime/include/chips/pulp/config.h \
+ /home/abdelhay21/GP/pulp-runtime/include/archi/pulp_defs.h \
+ /home/abdelhay21/GP/pulp-runtime/lib/libc/minimal/include/string.h \
+ /home/abdelhay21/GP/pulp-runtime/lib/libc/minimal/include/ctype.h
+
+/home/abdelhay21/GP/pulp-runtime/include/chips/pulp/config.h:
+
+/home/abdelhay21/GP/pulp-runtime/include/archi/pulp_defs.h:
+
+/home/abdelhay21/GP/pulp-runtime/lib/libc/minimal/include/string.h:
+
+/home/abdelhay21/GP/pulp-runtime/lib/libc/minimal/include/ctype.h:
---
+++ V2.4/FFT_HWPE/test_rt_dma_FFT_SYN/build/test_FFT/kernel/irq_asm.d
@@ -0,0 +1,8 @@
+/home/abdelhay21/GP/rtl/my_ips/FFT_HWPE/test_rt_dma_FFT_SYN/build/test_FFT/kernel/irq_asm.o: \
+ /home/abdelhay21/GP/pulp-runtime/kernel/irq_asm.S \
+ /home/abdelhay21/GP/pulp-runtime/include/chips/pulp/config.h \
+ /home/abdelhay21/GP/pulp-runtime/include/archi/pulp_defs.h
+
+/home/abdelhay21/GP/pulp-runtime/include/chips/pulp/config.h:
+
+/home/abdelhay21/GP/pulp-runtime/include/archi/pulp_defs.h:
